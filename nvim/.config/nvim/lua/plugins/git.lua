f7a4746d62e78d2c9dc24e111d0f53df4afb4acf
srj-17 <saugatrijal17@gmail.com>
2024-10-28 14:12:10 +0545

    don't diffview

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -15,20 +15,21 @@ return {
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         end
     },
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",         -- required
-            "sindrets/diffview.nvim",        -- optional - Diff integration
-
-            -- Only one of these is needed.
-            "nvim-telescope/telescope.nvim", -- optional
-            "ibhagwan/fzf-lua",              -- optional
-            "echasnovski/mini.pick",         -- optional
-        },
-        config = function ()
-            local neogit = require("neogit")
-            vim.keymap.set("n", "<leader>gs", function() neogit.open({ kind = "floating" }) end, { silent = true, noremap = true })
-        end
-    },
+    -- {
+        -- -- neogit seems unintuitive, so, not yet
+    --     "NeogitOrg/neogit",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",         -- required
+    --         "sindrets/diffview.nvim",        -- optional - Diff integration
+    --
+    --         -- Only one of these is needed.
+    --         "nvim-telescope/telescope.nvim", -- optional
+    --         "ibhagwan/fzf-lua",              -- optional
+    --         "echasnovski/mini.pick",         -- optional
+    --     },
+    --     config = function ()
+    --         local neogit = require("neogit")
+    --         vim.keymap.set("n", "<leader>gs", function() neogit.open({ kind = "floating" }) end, { silent = true, noremap = true })
+    --     end
+    -- },
 }
