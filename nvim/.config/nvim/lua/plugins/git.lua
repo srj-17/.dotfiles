ab405e1b69cf1c069fa6612f6c78389218b3bc76
srj-17 <saugatrijal17@gmail.com>
2024-10-28 13:33:57 +0545

    Occasional commit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,11 +8,28 @@ return {
             vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<Enter>")
         end
     },
+    -- {
+    --     -- wrap git commands so that you can do those from neovim
+    --     "tpope/vim-fugitive",
+    --     config = function ()
+    --         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    --     end
+    -- },
+
     {
-        -- wrap git commands so that you can do those from neovim
-        "tpope/vim-fugitive",
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",              -- optional
+            "echasnovski/mini.pick",         -- optional
+        },
         config = function ()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            local neogit = require("neogit")
+            vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
         end
     },
 }
