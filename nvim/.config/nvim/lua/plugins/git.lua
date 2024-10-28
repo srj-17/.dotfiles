aa3a5a14c4e3e3c4a6363fa5fd96ad3647c781f0
srj-17 <saugatrijal17@gmail.com>
2024-10-28 13:48:12 +0545

    Trying out neogit,
    Misses many other features, of vim fugitive,
    thus, don't know if I'll change so soon

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,14 +8,13 @@ return {
             vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<Enter>")
         end
     },
-    -- {
-    --     -- wrap git commands so that you can do those from neovim
-    --     "tpope/vim-fugitive",
-    --     config = function ()
-    --         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    --     end
-    -- },
-
+    {
+        -- wrap git commands so that you can do those from neovim
+        "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end
+    },
     {
         "NeogitOrg/neogit",
         dependencies = {
@@ -29,7 +28,7 @@ return {
         },
         config = function ()
             local neogit = require("neogit")
-            vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
+            vim.keymap.set("n", "<leader>gs", function() neogit.open({ kind = "floating" }) end, { silent = true, noremap = true })
         end
     },
 }
