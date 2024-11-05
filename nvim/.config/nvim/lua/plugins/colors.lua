e6ca8bec4e8ab53b3566429f5ad5d11c8992d204
srj-17 <saugatrijal17@gmail.com>
2024-11-05 12:59:19 +0545

    add gruvbox-material theme --- eye pleasing

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -2,7 +2,10 @@ return {
     {
         -- theme kanagawa
         "rebelot/kanagawa.nvim",
-        lazy = true,
+        dependencies = {
+            "nvim-lualine/lualine.nvim",
+        },
+        lazy = false,
         config = function()
             -- set theme catppucchin each time nvim starts
             -- vim.cmd.colorscheme("kanagawa-wave")
@@ -33,7 +36,7 @@ return {
                     comments = { },
                 },
             })
-            vim.cmd.colorscheme("tokyonight-night")
+            -- vim.cmd.colorscheme("tokyonight-night")
             require("lualine").setup({
                 options = { theme = "auto" },
              })
@@ -41,7 +44,7 @@ return {
     },
 	{
 		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
+		lazy = false,
 		priority = 1000,
 		opts = function()
 			return {
@@ -52,28 +55,52 @@ return {
             -- vim.cmd.colorscheme("solarized-osaka")
         end
 	},
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     lazy = false,
+    --     dependencies = {
+    --         "nvim-lualine/lualine.nvim",
+    --     },
+    --     priority = 1000,
+    --     config = function()
+    --         require("gruvbox").setup({
+    --             transparent_mode = false,
+    --             italic = {
+    --                 functions = true,
+    --                 keywords = false,
+    --             },
+    --         })
+    --
+    --         vim.o.background = "dark" -- dark for dark, and light for light mode
+    --
+    --         -- vim.cmd.colorscheme("gruvbox")
+    --
+    --         require("lualine").setup({
+    --             options = { theme = "gruvbox" },
+    --         })
+    --     end,
+    -- },
     {
-        "morhetz/gruvbox",
-        lazy = true,
-        config = function()
-        --     vim.cmd.colorscheme("gruvbox");
-        --
-        --     -- this is for transparency 
-        --     -- credit to theprimeagen
-        --     function ColorMyPencils(color) 
-        --         color = color or "gruvbox"
-        --         vim.cmd.colorscheme(color)
-        --
-        --         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        --         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        --
-        --     end
-        --     ColorMyPencils()
-        --
-        -- -- and then set the lualine for gruvbox
-        -- require("lualine").setup({
-        --     options = { theme = "gruvbox" },
-        -- })
-        end
-    },
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1001,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.g.gruvbox_material_better_performance = 1
+        vim.g.gruvbox_material_enable_bold = 1
+        vim.g.gruvbox_material_enable_italic = 1
+        vim.g.gruvbox_material_transparent_background = 1
+        vim.g.gruvbox_material_dim_inactive_windows = 1
+
+        vim.cmd.colorscheme("gruvbox-material")
+
+        require("lualine").setup({
+          options = {
+            theme = "gruvbox-material"
+          }
+        })
+      end
+    }
 }
