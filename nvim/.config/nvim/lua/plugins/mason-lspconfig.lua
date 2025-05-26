e1474470d420efd8cadd6efea08c46520a2eaca7
srj-17 <saugatrijal17@gmail.com>
2025-05-26 17:52:55 +0545

    feat: make lsp work for php

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,13 +1,14 @@
 return {
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		dependencies = {
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
 		config = function()
 			require("mason").setup()
-			local mason_tool_installer = require("mason-tool-installer")
+
 			-- especially for linters and formatters
+			local mason_tool_installer = require("mason-tool-installer")
 			mason_tool_installer.setup({
 				ensure_installed = {
 					"prettier", -- prettier formatter
@@ -16,61 +17,22 @@ return {
 					-- "black", -- python formatter
 					"pylint", -- python linter
 					"eslint_d", -- js linter
-					"lua_ls",
-					"clangd",
-					"markdown_oxide",
-					"ts_ls",
-					"cssls",
-					"tailwindcss",
-					"phpactor",
 				},
 			})
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
-			"mason.nvim",
+			"mason-org/mason.nvim",
 			-- this is for broadcasting to the lsp about our
 			"hrsh7th/cmp-nvim-lsp",
+			"neovim/nvim-lspconfig",
 		},
 		config = function()
-			require("mason-lspconfig").setup({
-				automatic_installation = true,
-				-- this setup will read all data from mason and
-				-- pull the list of all installed servers
-				-- will pass items on that list to handlers who will setup the servers
-				-- one by one
-				handlers = {
-					function(server_name)
-						local capabilities = require("cmp_nvim_lsp").default_capabilities()
-						require("lspconfig")[server_name].setup({
-							-- Also, setup up completion configuration provided by cmp-nvim-lsp for each server
-							capabilities = capabilities,
+			local mason_lspconfig = require("mason-lspconfig")
 
-							-- this is for go to definition setup
-							on_attach = function(client, bufnr)
-								local opts = { buffer = bufnr, remap = false }
-								vim.keymap.set("n", "gd", function()
-									vim.lsp.buf.definition()
-								end, opts)
-								vim.keymap.set("n", "K", function()
-									vim.lsp.buf.hover()
-								end, opts)
-								vim.keymap.set("n", "<leader>gr", function()
-									vim.lsp.buf.references()
-								end, opts)
-								vim.keymap.set("n", "<leader>ca", function()
-									vim.lsp.buf.code_action()
-								end, opts)
-								-- rename a variable throughout the whole project
-								vim.keymap.set("n", "<leader>rn", function()
-									vim.lsp.buf.rename()
-								end, opts)
-							end,
-						})
-					end,
-				},
+			mason_lspconfig.setup({
 				ensure_installed = {
 					"ts_ls",
 					"html",
@@ -85,6 +47,54 @@ return {
 					"phpactor",
 				},
 			})
+
+			mason_lspconfig.setup_handlers({
+				-- this setup will read all data from mason and
+				-- pull the list of all installed servers
+				-- will pass items on that list to handlers who will setup the servers
+				-- one by one
+
+				function(server_name)
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+					require("lspconfig")[server_name].setup({
+						-- Also, setup up completion configuration provided by cmp-nvim-lsp for each server
+						capabilities = capabilities,
+
+						-- this is for go to definition setup
+						on_attach = function(client, bufnr)
+							local opts = { buffer = bufnr, remap = false }
+						end,
+					})
+				end,
+
+				-- necessary because idk why root directory was
+				-- not being detected for phpactor during :checkhealth lspconfig
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/phpactor.lua
+				["phpactor"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.phpactor.setup({
+						root_dir = vim.fn.getcwd(),
+					})
+				end,
+			})
+
+			local opts = { silent = true, noremap = true }
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover()
+			end, opts)
+			vim.keymap.set("n", "<leader>gr", function()
+				vim.lsp.buf.references()
+			end, opts)
+			vim.keymap.set("n", "<leader>ca", function()
+				vim.lsp.buf.code_action()
+			end, opts)
+			-- rename a variable throughout the whole project
+			vim.keymap.set("n", "<leader>rn", function()
+				vim.lsp.buf.rename()
+			end, opts)
 		end,
 	},
 }
